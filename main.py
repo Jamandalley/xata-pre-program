331c4a5dcd3b55227c0ab4b8cddd8d9df48aaa6f
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-08 09:46:19 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+# The fastAPI module for collecting user full name 
+# This is a Python module for a FastAPI web application. 
+# It includes a router for handling PDF-related requests, which is imported from the getpdf module. 
+# The FastAPI app is created using FastAPI(), and the PDF router is included in the app using app.include_router().
+
+# You can find more information about FastAPI and its features in the official documentation: https://devdocs.io/fastapi/.
