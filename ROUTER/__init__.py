edb1906f16d844feaeea9f645f8f6b9e98d80fe0
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-08 09:42:55 +0100

    Update __init__.py

--- ROUTER/__init__.py
+++ ROUTER/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from .getpdf import router as pdf_router
+from .getpdf import router as name_router
 
 app = FastAPI()
 
-app.include_router(pdf_router)
+app.include_router(name_router)
