90613e348970d4321c119dcf5b28a4e66f25c2b7
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-14 10:12:30 +0100

    Update getname.py

--- ROUTER/getname.py
+++ ROUTER/getname.py
@@ -1,1 +1,5 @@
 
+@router.get("/user/{first_name}/{last_name}")
+async def get_full_name(first_name: str, last_name: str):
+    full_name = f"{first_name} {last_name}"
+    return {"full_name": full_name}
