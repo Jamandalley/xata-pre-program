4638984ac903dc29978dceb417a18f8d5a760baa
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-14 10:46:16 +0100

    Update getname.py

--- ROUTER/getname.py
+++ ROUTER/getname.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the myAPI. Use /user/{first_name}/{last_name} to get full name or /upload_pdf/ to upload a PDF."}
+
 @router.get("/user/{first_name}/{last_name}")
 async def get_full_name(first_name: str, last_name: str):
     full_name = f"{first_name} {last_name}"
