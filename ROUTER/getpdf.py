68439185dfef55526fdd5de9298aa2c1874372f4
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-14 10:11:54 +0100

    Update getpdf.py

--- ROUTER/getpdf.py
+++ ROUTER/getpdf.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/user/{first_name}/{last_name}")
-async def get_full_name(first_name: str, last_name: str):
-    full_name = f"{first_name} {last_name}"
-    return {"full_name": full_name}
+@router.post("/upload_pdf/")
+async def upload_pdf(pdf_file: UploadFile = File(...)):
+    save_directory = "pdf_files"
+    os.makedirs(save_directory, exist_ok=True)
+    file_path = os.path.join(save_directory, pdf_file.filename)
+    with open(file_path, "wb") as buffer:
+        buffer.write(await pdf_file.read())
+    return {"message": "PDF file uploaded successfully", "file_name": pdf_file.filename}
