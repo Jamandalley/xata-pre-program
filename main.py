f7e0749009a00fad783c2e735ba8cbfd79411d53
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-14 10:49:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,7 @@
 # The FastAPI app is created using FastAPI(), and the PDF router is included in the app using app.include_router().
 
 # You can find more information about FastAPI and its features in the official documentation: https://devdocs.io/fastapi/.
+
+# Today 14/02/2024, created an endpoint to allow user upload pdf files and save directly to the local disk. The endpoint is contained in getpdf.py
+# name: Abimbade Jamiu A.
+# email: jamiuabimbade@gmail.com
