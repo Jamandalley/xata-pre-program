3358ee96d644ba6a14c2dbb099682387e113d1ef
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-22 15:10:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # API Documentation:  https://fastapi.tiangolo.com
 import uvicorn
 from fastapi import FastAPI
-from routers import get_fullname, get_pdf
+from routers import getname, getpdf
 
 app = FastAPI()
 
-app.include_router(get_fullname.router)
-app.include_router(get_pdf.router)
+app.include_router(getname.router)
+app.include_router(getpdf.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
