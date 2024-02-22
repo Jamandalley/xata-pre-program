6b7ff54d58609d2bcb19ee1012ad0ef1444a6d98
Abimbade jamiu <139856697+Jamandalley@users.noreply.github.com>
2024-02-22 15:56:35 +0100

    Update main.py

--- ROUTER/main.py
+++ ROUTER/main.py
@@ -1,3 +1,7 @@
+# Author Name: Abimbade Jamiu A., 
+# email: jamiuabimbade@gmail.com
+# API Documentation:  https://fastapi.tiangolo.com
+
 from fastapi import FastAPI
 from .getpdf import router as get_pdf
 
